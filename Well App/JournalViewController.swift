e42662715d8a924b3c491289eaa4e638b359fa07
Diya Patel <diyatpatel2024@gmail.com>
2022-07-19 13:38:37 -0500

    added buttons to all 3 view controllers

---
+++ Well App/JournalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JournalViewController.swift
+//  Well App
+//
+//  Created by Diya Patel on 7/19/22.
+//
+
+import UIKit
+
+class JournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
