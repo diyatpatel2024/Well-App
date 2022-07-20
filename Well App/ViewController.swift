b39780b18b7c010ab7a8b103c5f4e14b3647e8d2
Rebecca De La Garza <rebeccadelagarzamellado@gmail.com>
2022-07-20 10:54:45 -0500

    made the quotes button work

--- Well App/ViewController.swift
+++ Well App/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    let randomQuote = ["You don't always need to understand your journey in life, you just need to trust that you're going in the right direction. -Steven Aitchison", "If you fell down yesterday, stand up today. - H.G. Wells", "The life in front of you is far more important than the life behind you. -Joel Osteen", "There is no real beauty without some slight imperfection. -James Salter", “It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest -Walter Anderson” , "Only in the darkness can you see the stars. -Dr. Martin Luther King Jr.", "I have found that if you love life, life will love you back. -Arthur Rubinstein"]
+    let randomQuote = ["You don't always need to understand your journey in life, you just need to trust that you're going in the right direction. -Steven Aitchison", "If you fell down yesterday, stand up today. - H.G. Wells", "The life in front of you is far more important than the life behind you. -Joel Osteen", "There is no real beauty without some slight imperfection. -James Salter", "It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest -Walter Anderson", "Only in the darkness can you see the stars. -Dr. Martin Luther King Jr.", "I have found that if you love life, life will love you back. -Arthur Rubinstein"]
 
 
     @IBAction func quotes(_ sender: Any) {
@@ -24,5 +24,6 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet weak var generatedQuote: UILabel!
+    
 }
 
