75833efdf5240cd8f3cbc6dc37c0f00159eaa8e7
Diya Patel <diyatpatel2024@gmail.com>
2022-07-20 10:19:06 -0500

    quote button/label

--- Well App/ViewController.swift
+++ Well App/ViewController.swift
@@ -13,7 +13,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    let randomQuote = ["You don't always need to understand your journey in life, you just need to trust that you're going in the right direction. -Steven Aitchison", "If you fell down yesterday, stand up today. - H.G. Wells", "The life in front of you is far more important than the life behind you. -Joel Osteen", "There is no real beauty without some slight imperfection. -James Salter", “It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest -Walter Anderson” , "Only in the darkness can you see the stars. -Dr. Martin Luther King Jr.", "I have found that if you love life, life will love you back. -Arthur Rubinstein"]
 
 
+    @IBAction func quotes(_ sender: Any) {
+        let randomNum = Int.random (in: 0...(randomQuote.count-1))
+            generatedQuote.text = randomQuote[randomNum]
+
+    }
+    
+    @IBOutlet weak var generatedQuote: UILabel!
 }
 
