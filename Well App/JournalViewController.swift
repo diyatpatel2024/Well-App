3bda1d1ba22ae01038362fdee07f1ebed80032df
Diya Patel <diyatpatel2024@gmail.com>
2022-07-21 10:13:33 -0500

    added text view

--- Well App/JournalViewController.swift
+++ Well App/JournalViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class JournalViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
